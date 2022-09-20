e48d8fe1e73ec8df3b62542d03743114dbe88e2c
vidhya-a-mariappan <112065733+vidhya-a-mariappan@users.noreply.github.com>
2022-09-20 19:13:16 +0530

    Create mainAbstract.java

---
+++ mainAbstract.java
@@ -0,0 +1,11 @@
+public class mainAbstract {
+   public static void main(String[] args) {
+        joining myjoin = new joining();
+        System.out.println("Name: " + myjoin.name);
+        System.out.println("Email: " + myjoin.email);
+        System.out.println("Age: " +myjoin.age);
+        System.out.println("Joining Year: " + myjoin.yearOfJoining);
+       myjoin.work();
+   }
+
+}
